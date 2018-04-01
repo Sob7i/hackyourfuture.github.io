fed935c2f00ff4020fe222b6b07402aae99c4988
ideabile <info@ideabile.com>
2018-04-01 22:25:52 +0200

    🚀 Add lambda function and dependecies to compile it

---
+++ api-lambda/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    entry: `${__dirname}/main.js`,
+    output: {
+        filename: '[name].js',
+        libraryTarget: 'commonjs2',
+        path: path.join(__dirname, "dist")
+    },
+    target: "node",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env'],
+                        plugins: [
+                            ["transform-runtime", { "polyfill": false }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
