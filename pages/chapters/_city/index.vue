c1a1ac887a77a393eebc97809a95bc817c28ff10
Maarten Vleugels <maartenvleugels@Maartens-MacBook-Pro.local>
2018-05-31 17:16:50 +0200

    added data for chapters

--- pages/chapters/_city/index.vue
+++ pages/chapters/_city/index.vue
@@ -1,15 +1,23 @@
 <template>
 <div>
-  <main class="Chapter container">
-    <div class="Chapter__header">
+  <main class="Chapter ">
+    <div class="Chapter__header container">
         <div v-html="welcome"></div>
         <div class="Chapter__header-image">
             <img src="/gallery/03.jpg">
         </div>
         <div class="Chapter__header-description" v-html="description"></div>
     </div>
+    <section class="Chapter__visit">
+        <div class="Chapter__visit-image">
+            <img :src="'/chapters/' + city + '.png'">
+        </div>
+        <div class="Chapter__visit-description" v-html="visit">
+
+        </div>
+    </section>
   </main>
-  <Signup  :color="'grey'"/>
+  <Signup :color="'grey'"/>
 </div>
 </template>
 
@@ -21,20 +29,26 @@ import Signup from '~/components/signup/Signup';
          let city = params.city
          let welcome
          let description
+         let visit
          try {
              let req  = await axios.get('/content/en/chapters/' + city + '/welcome.json')
              let req1 = await axios.get('/content/en/chapters/' + city + '/description.json')
+             let req2 = await axios.get('/content/en/chapters/' + city + '/visit.json')
              welcome = req.data.body
              description = req1.data.body
+             visit = req2.data.body
          } catch (e) {
              console.log(e)
              welcome = false
              description = false
+             visit = false
          }
          return {
              siteKey: "6LfsWVAUAAAAAE5mdeB0ICRoDDkWJd00vr9NEZ3I",
              welcome: welcome ? welcome : null,
-             description: description ? description : null
+             city: city,
+             description: description ? description : null,
+             visit: visit ? visit : null
          }
      },
      components: {
@@ -45,7 +59,32 @@ import Signup from '~/components/signup/Signup';
 
 <style lang="scss">
 .Chapter {
-    margin: 5%;
-    padding: $base-vertical-rithm;
+    &__header {
+        margin: 5%;
+        padding: $base-vertical-rithm;
+        background: $color-grey;
+    }
+    &__visit {
+        padding: $base-vertical-rithm * 20;
+        background: $color-purple;
+        width: 100%;
+        &-image {
+            width: 50%;
+            display: inline-block;
+            vertical-align: top;
+        }
+        &-description {
+            width: 50%;
+            display: inline-block;
+            padding-left: $base-vertical-rithm * 20;
+            h1,h2,h3,p {
+                color: white;
+            }
+            h1 {
+                font-size: 44px;
+                line-height: 44px;
+            }
+        }
+    }
 }
 </style>
