e80893cfdb8cbaecffa9dfe074d9cd1aaecab7d8
Sarbast <sarbast2009@hotmail.co.uk>
2018-08-03 01:12:33 +0200

    fixed the focusing issue
    the user now will see non-empty inputs have active label

--- pages/apply/index.vue
+++ pages/apply/index.vue
@@ -107,7 +107,11 @@
             e.target.parentNode.classList.add('active'); 
         }
      },
-     components: {
+     mounted: function(){
+         this.$el.querySelectorAll('.input').forEach(function(i) {
+                if(i.value.length == 0) { i.parentNode.classList.remove('active');}
+                else{ i.parentNode.classList.add('active');}
+            });
      }
  }
 </script>
