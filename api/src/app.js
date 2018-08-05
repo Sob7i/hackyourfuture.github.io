f14476c27ea4a8154d7572e6d4d7c8c5341b22b0
Alexandre Borrego <contact@alxbrg.com>
2018-08-05 16:57:53 +0200

    Updated API to new specs

--- api/src/app.js
+++ api/src/app.js
@@ -5,8 +5,7 @@ const cors = require('cors');
 const {
     Apply,
     ContactUs,
-    SubmitCV,
-    SubmitAssignment,
+    Upload,
 } = require('./middlewares');
 
 const app = express();
@@ -45,7 +44,6 @@ app.post('/apply', (req, res) => {
 });
 app.post('/contact-us', (req, res) => ContactUs(req, res));
 app.post('/apply', (req, res) => Apply(req, res));
-app.put('/cv', (req, res) => SubmitCV(req, res));
-app.put('/assignment', (req, res) => SubmitAssignment(req, res));
+app.post('/upload', (req, res) => Upload(req, res));
 
 module.exports = app;
