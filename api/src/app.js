cb507ad3f06c521dbf1ed9dc1fdf16fe129c2be2
hadeel alabrash <hadeelalabrash@hadeels-air.home>
2018-08-07 00:38:20 +0200

    hadeel team a

--- api/src/app.js
+++ api/src/app.js
@@ -9,9 +9,18 @@ const {
 
 const app = express();
 
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const FileUpload = Upload.fields(
+    [{
+        name: 'files',
+        maxCount: 1
+    }]
+);
 
 app.use((req, res, next) => {
     console.log('--- Debug request ---');
